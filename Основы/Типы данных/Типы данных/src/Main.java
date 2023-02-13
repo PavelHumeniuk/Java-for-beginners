9777b59cf4a9f97bdd6fbe2d2ec885688ca4d89f
pavelhumeniuk <phuman911@gmail.com>
2023-02-13 21:58:07 +0100

    added data types

--- Основы/Типы данных/Типы данных/src/Main.java
+++ Основы/Типы данных/Типы данных/src/Main.java
@@ -1,5 +1,28 @@
 public class Main {
    public static void main(String[] args) {
-      // Write your solution here
+      // целочисленные значения
+      byte a = -5;
+      short b = 200;
+      int c = 50000;
+      long d = 20;
+      System.out.println(a);
+      System.out.println(b);
+      System.out.println(c);
+      System.out.println(d);
+
+      // числа с плавающей точкой
+      double e = 12.5;
+      // у float нужно обязательно в конце писать f
+      float f = 17.3f;
+      System.out.println(e);
+      System.out.println(f);
+
+      // символы
+      char j = 'j';
+      System.out.println(j);
+
+      // логические значения
+      boolean k = false;
+      System.out.println(k);
    }
 }
