9cc9d4ca485db9d6abd9075956a32ceabba9f5f1
pavelhumeniuk <phuman911@gmail.com>
2023-02-15 22:27:47 +0100

    added task for numbers

---
+++ Основы/Типы данных/Числа/src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+   public static void main(String[] args) {
+      int a = 5;
+      int b = 6;
+
+      // результат вычислений можно записать в новую переменную
+      int c = a + b;
+
+      System.out.println(c);
+
+      // а можно сразу напечатать
+      System.out.println(a * c);
+      System.out.println(c / a);
+
+      // попробуй ниже перемножить все 3 переменные (a, b, c) и напечатать в консоль
+
+   }
+}
