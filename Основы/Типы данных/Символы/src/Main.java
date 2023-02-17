46834679fa1ffe8af1edaf41b3dcdbe899cc4ad4
pavelhumeniuk <phuman911@gmail.com>
2023-02-17 21:31:39 +0100

    added char

---
+++ Основы/Типы данных/Символы/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        char letter = 'a';
+        char digit = '7';
+        System.out.println(letter);
+        System.out.println(digit);
+    }
+}
