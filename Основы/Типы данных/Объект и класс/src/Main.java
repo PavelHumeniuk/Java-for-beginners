e6f715467b79fb93a2ff6d4d1196b8b646977c5d
pavelhumeniuk <phuman911@gmail.com>
2023-03-06 20:18:12 +0100

    Add class and object

--- Основы/Типы данных/Объект и класс/src/Main.java
+++ Основы/Типы данных/Объект и класс/src/Main.java
@@ -1,5 +1,33 @@
 public class Main {
    public static void main(String[] args) {
-      // Write your solution here
+      // Создаем объект типа Pizza c именем pepperoni
+      // new означает, что мы создаем новый объект вызывая его конструктор
+      // В конструктор мы передали характеристики нашей пиццы: имя и ее вес.
+      Pizza pepperoni = new Pizza("Пепперони", 600);
+
+      // Напечатаем в консоли характеристики пиццы
+      System.out.println(pepperoni.name);
+      System.out.println(pepperoni.weight);
+
+      // Попробуй ниже создать пиццу Маргарита с весом 450 грамм
+
+
+
+      // Вывести в консоль ее характеристики
+
+   }
+
+   // Опишем класс пицца. Для простоты он будет состоять из имени и веса
+   // Можно добавить еще сколько угодно характеристик внутри класса,
+   // но для начала ограничимся таким примером
+   public static class Pizza {
+      String name;
+      int weight;
+
+      // Это называется конструктор, своеобразная инструкция, по которой мы создадим объект
+      public Pizza(String name,  int weight) {
+         this.name = name;
+         this.weight = weight;
+      }
    }
 }
