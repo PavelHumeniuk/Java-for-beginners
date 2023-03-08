769bc6f07f3b15d286c718ea885d033fc87c0660
pavelhumeniuk <phuman911@gmail.com>
2023-03-08 20:51:08 +0100

    added if else

--- Основы/Условные операторы/If else/src/Main.java
+++ Основы/Условные операторы/If else/src/Main.java
@@ -1,5 +1,29 @@
 public class Main {
-   public static void main(String[] args) {
-      // Write your solution here
-   }
+    public static void main(String[] args) {
+        int age = 23;
+        int allowedAge = 16;
+
+        boolean isAllowedToUse = age > allowedAge;
+
+        // Если разрешено пользоваться, то
+        if (isAllowedToUse) {
+            System.out.println("Запускаем программу!");
+            // Иначе
+        } else {
+            System.out.println("Выход.");
+        }
+
+        System.out.println("-------");
+        boolean isAdmin = true;
+
+        // Таким же образом можно сделать сколько угодно ветвлений
+        // Просто добавляем if else для каждого нового условия
+        if (isAdmin) {
+            System.out.println("Запускаем конфигурацию проекта!");
+        } else if (isAllowedToUse) {
+            System.out.println("Запускаем программу!");
+        } else {
+            System.out.println("Выход.");
+        }
+    }
 }
