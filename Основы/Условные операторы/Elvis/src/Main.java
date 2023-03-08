c0dd61b3b1a1c809d520c0b96c424d529c4836bc
pavelhumeniuk <phuman911@gmail.com>
2023-03-08 21:03:00 +0100

    added elvis

--- Основы/Условные операторы/Elvis/src/Main.java
+++ Основы/Условные операторы/Elvis/src/Main.java
@@ -1,5 +1,22 @@
 public class Main {
    public static void main(String[] args) {
-      // Write your solution here
+      // Возьмем пример из предыдущего урока...
+      int age = 23;
+      int allowedAge = 16;
+      boolean isAllowedToUse = age > allowedAge;
+
+      String message;
+      if (isAllowedToUse) {
+         message = "Запускаем программу!";
+      } else {
+         message = "Выход.";
+      }
+      System.out.println(message);
+      System.out.println("-------");
+
+      // ... и перепишем:
+      // сначала идет значение, если условие верно (true), затем если ложь (false)
+      String elvisMessage = isAllowedToUse ? "Запускаем программу!" :  "Выход.";
+      System.out.println(elvisMessage);
    }
 }
